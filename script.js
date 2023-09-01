690b35f1f4e3ab5f7b738c9a01e5fc12343575e6
sakibsarkar <sakibsarkar707@gmail.com>
2023-09-01 20:00:13 +0600

    content not found added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let sortID = 0;
+
 const loadData = async () => {
     const res = await fetch("https://openapi.programming-hero.com/api/videos/categories")
     const data = await res.json()
@@ -7,66 +8,94 @@ const loadData = async () => {
     info.forEach(categorey => {
         let button = document.createElement("button");
         button.setAttribute("onclick", `loadcontent("${categorey.category_id}")`)
+        button.classList.add("catBTN")
         button.innerText = categorey.category
         buttonContainer.appendChild(button)
-        
+
     })
 }
 
 loadData()
-const loadcontent = async (id) => {
-sortID = id
+const Default = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const data = await res.json()
     let ContentData = data.data;
-
-    let parent = document.querySelector(".videos");
-    parent.innerHTML = ""
-    ContentData.forEach((content) => {
-        // video duration
-        let duration = content.others?.posted_date
-        if (duration != "") {
-            let durationInMilisec = duration * 1000
-            var hr = Math.floor(durationInMilisec / (60 * 60 * 1000)) + "hrs "
-            var min = Math.floor(durationInMilisec % (60 * 60 * 1000) / (60 * 1000)) + "min ago"
-        }
-        else {
-            var hr = ""
-            var min = ""
-        }
-
-
-        // parent div
-        let thubnail = content.thumbnail;
-        let tittle = content.title
-        let authorPic = content.authors[0].profile_picture;
-        let profileName = content.authors[0].profile_name;
-        let isVarifed = content.authors[0].verifed
-        let view = content.others?.views
-        let card = document.createElement("div")
-        card.classList.add("videoCard")
-        card.innerHTML = `
+}
+const loadcontent = async (id) => {
+    sortID = id
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    const data = await res.json()
+    let ContentData = data.data;
+    console.log(ContentData.length)
+    if (ContentData.length !== 0) {
+            document.querySelector(".items").innerHTML = ""
+                let parent = document.createElement("div");
+                parent.classList.add("videos")
+                
+                ContentData.forEach((content) => {
+                    // video duration
+                    let duration = content.others?.posted_date
+                    if (duration != "") {
+                        let durationInMilisec = duration * 1000
+                        var hr = Math.floor(durationInMilisec / (60 * 60 * 1000)) + "hrs "
+                        var min = Math.floor(durationInMilisec % (60 * 60 * 1000) / (60 * 1000)) + "min ago"
+                    }
+                    else {
+                        var hr = ""
+                        var min = ""
+                    }
+                    
+                    
+                    // parent div
+                    let thubnail = content.thumbnail;
+                    let tittle = content.title
+                    let authorPic = content.authors[0].profile_picture;
+                    let profileName = content.authors[0].profile_name;
+                    let isVarifed = content.authors[0].verifed
+                    let view = content.others?.views
+                    let card = document.createElement("div")
+                    card.classList.add("videoCard")
+                    card.innerHTML = `
                     <div class="thubnailConatiner">
                     <p class="time">${hr}${min}<p>
                     <img src="${thubnail}" alt="">
                     </div>
                     <div class="info">
-                        <div class="leftPart">
-                            <img src="${authorPic}" alt="">
-                        </div>
-                        <div class="rightPart">
-                            <h1>${tittle}</h1>
-                            <p>${profileName}</p>
-                            <p>${view}</p>
-                        </div>
-
+                    <div class="leftPart">
+                    <img src="${authorPic}" alt="">
                     </div>
-                
-        
-        `
-        parent.appendChild(card)
+                    <div class="rightPart">
+                    <h1>${tittle}</h1>
+                    <p>${profileName}</p>
+                    <p>${view}</p>
+                    </div>
+                    
+                    </div>
+                    
+                    
+                    `
+                    parent.appendChild(card)
+                    document.querySelector(".items").appendChild(parent)
 
-    })
+        })
+
+
+    }
+    else {
+        let parent = document.querySelector(".videos");
+        parent.innerHTML = "";
+
+        let errorDiv = document.createElement("div")
+        errorDiv.classList.add("errorContainer")
+        errorDiv.innerHTML = `
+        <div class="error">
+        <img src="images/Icon.png" alt="">
+        <h1>Oops!! Sorry, There is no content here</h1>
+    </div>
+`
+document.querySelector(".items").appendChild(errorDiv)
+        
+    }
 
 }
 
@@ -98,8 +127,8 @@ const sort = () => {
                 var hr = ""
                 var min = ""
             }
-    
-    
+
+
             // parent div
             let thubnail = content.thumbnail;
             let tittle = content.title
@@ -128,10 +157,10 @@ const sort = () => {
                     
             
             `
-        
+
             parent.appendChild(card);
         });
     };
-loadSortedcontent()
+    loadSortedcontent()
 }
 
